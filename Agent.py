818bceae2fa2f3777d38d56de8dea479e940203c
isaac <ikaufman@gatech.edu>
2019-03-30 11:58:31 -0400

    much cleaner

--- Agent.py
+++ Agent.py
@@ -13,47 +13,32 @@ localization_out_queue = Queue()
 #     REQUEST_DATA = 1
 #     PUT_DATA = 2
 
-def vision_reader(inQ):
+def vision(inQ):
     pass
-def localization_reader(inQ):
+def localization(inQ, outQ):
     pass
-def localization_writer(outQ):
+def pathfinding(inQ, outQ):
     pass
-def pathfinding_reader(inQ):
-    pass
-def pathfinding_writer(outQ):
-    pass
-def motor_control_reader(inQ):
-    pass
-def motor_control_writer(outQ):
+def motor_control(inQ, outQ):
     pass
 
 if __name__ == '__main__':
 
     # initialize and start processes
-    vision_reader_proc = Process(target=vision_reader, args=((vision_in_queue),))
-    localization_reader_proc = Process(target=localization_reader, args=((localization_in_queue),))
-    localization_writer_proc = Process(target=localization_writer, args=((localization_out_queue),))
-    pathfinding_reader_proc = Process(target=pathfinding_reader, args=((pathfinding_in_queue),))
-    pathfinding_writer_proc = Process(target=pathfinding_writer, args=((pathfinding_out_queue),))
-    motor_control_reader_proc = Process(target=motor_control_reader, args=((motor_control_in_queue),))
-    motor_control_writer_proc = Process(target=motor_control_writer, args=((motor_control_out_queue),))
+    vision_proc = Process(target=vision, args=((vision_in_queue),))
+    localization_proc = Process(target=localization, args=((localization_in_queue),(localization_out_queue),))
+    pathfinding_proc = Process(target=pathfinding, args=((pathfinding_in_queue),(pathfinding_out_queue),))
+    motor_control_proc = Process(target=motor_control, args=((motor_control_in_queue),(motor_control_out_queue),))
 
-    vision_reader_proc.daemon = True
-    localization_reader_proc.daemon = True
-    localization_writer_proc.daemon = True
-    pathfinding_reader_proc.daemon = True
-    pathfinding_writer_proc.daemon = True
-    motor_control_reader_proc.daemon = True
-    motor_control_writer_proc.daemon = True
+    vision_proc.daemon = True
+    localization_proc.daemon = True
+    pathfinding_proc.daemon = True
+    motor_control_proc.daemon = True
 
     print("Starting threads... ")
-    vision_reader_proc.start()
-    localization_reader_proc.start()
-    localization_writer_proc.start()
-    pathfinding_reader_proc.start()
-    pathfinding_writer_proc.start()
-    motor_control_reader_proc.start()
-    motor_control_writer_proc.start()
+    vision_proc.start()
+    localization_proc.start()
+    pathfinding_proc.start()
+    motor_control_proc.start()
     print("Done\n")
 
